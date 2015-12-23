8670f2a7c04f68d3ffa3cda9f59e3bd22fe306a8
unknown <mtillinger@LAT-7250-016.fourth.local>
2015-12-23 13:46:23 +0000

    Up to lesson 7

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,7 +1,71 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-/* 
+/*
+  App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+});
+
+/*
+  Header
+  <Header/>
+*/
+var Header = React.createClass({
+  render : function() {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+})
+
+/*
+  Order
+  <Order/>
+*/
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+})
+
+/*
+  Inventory
+  <Inventory/>
+*/
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
+
+/*
   StorePicker
   This will let us make <StorePicker/>
 */
@@ -20,4 +84,4 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
